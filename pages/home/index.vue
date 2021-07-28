d019ff9f25db12de5ecb9760b6044327f776a4f3
5coder-leo <19924519007@163.com>
2021-07-28 21:58:31 +0800

    更新测试

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,8 +3,8 @@
 
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">conduit</h1>
-        <p>A place to share your knowledge.</p>
+        <h1 class="logo-font">拉勾教育</h1>
+        <p>blog.5coder.cn</p>
       </div>
     </div>
 
